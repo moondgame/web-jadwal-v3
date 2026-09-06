c832bb39d134d513eedeac4ce91967ee90953f1b
moondgame <moondgame1@gmail.com>
2026-09-06 15:09:12 +0800

    firebase-config.js
    Updated Firebase configuration with new project credentials.

--- firebase-config.js
+++ firebase-config.js
@@ -8,10 +8,10 @@
 // keamanan data adalah "Firestore Security Rules", bukan menyembunyikan config ini.
 
 export const firebaseConfig = {
-  apiKey: "GANTI_DENGAN_API_KEY",
-  authDomain: "GANTI_DENGAN_PROJECT_ID.firebaseapp.com",
-  projectId: "GANTI_DENGAN_PROJECT_ID",
-  storageBucket: "GANTI_DENGAN_PROJECT_ID.appspot.com",
-  messagingSenderId: "GANTI_DENGAN_SENDER_ID",
-  appId: "GANTI_DENGAN_APP_ID"
+ apiKey: "AIzaSyDa5aVTwEr4nMcP_5oni4yG12wSBt-C_Ag",
+  authDomain: "jadwal-kuliah-ac220.firebaseapp.com",
+  projectId: "jadwal-kuliah-ac220",
+  storageBucket: "jadwal-kuliah-ac220.firebasestorage.app",
+  messagingSenderId: "177522212933",
+  appId: "1:177522212933:web:fe5bc34a645195599e632c"
 };
